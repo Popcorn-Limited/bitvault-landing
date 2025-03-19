1c6d1b4ee97953e21a6537867a4ba0e0606077e4
RedVeil <leon.niesler@gmail.com>
2025-03-19 11:02:30 +0100

    added community popover

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,9 +1,11 @@
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
 import Link from "next/link";
 
 export default function Navbar(): JSX.Element {
   return (
     <div className="">
       <div className="flex flex-row items-center justify-between w-full z-10 px-8 md:px-12 xl:px-24 py-4 bg-black border-b border-white/10 text-white">
+
         <Link
           href={`/`}
           passHref
@@ -18,14 +20,48 @@ export default function Navbar(): JSX.Element {
             BitVault
           </h2>
         </Link>
-        <Link
-          href={`https://docs.bitvault.finance/`}
-          passHref
-        >
-          <h2 className="text-xl leading-none hover:text-secondaryGold">
-            Docs
-          </h2>
-        </Link>
+
+        <div className="flex flex-row items-center gap-4">
+          <Link
+            href={`https://docs.bitvault.finance/`}
+            target="_blank"
+            passHref
+          >
+            <h2 className="text-xl leading-none hover:text-secondaryGold">
+              Docs
+            </h2>
+          </Link>
+          <Popover className="relative focus:outline-none text-center">
+            <PopoverButton className="focus:outline-none border-none">
+              <h2 className="text-xl leading-none hover:text-secondaryGold mt-1">
+                Community
+              </h2>
+            </PopoverButton>
+            <PopoverPanel anchor="bottom" className="flex flex-col mt-2 py-4 px-4 space-y-2 text-center text-white bg-customNeutral300 rounded-lg border border-white/10">
+              <Link
+                href={`https://x.com/BitVaultFinance`}
+                target="_blank"
+                passHref
+              >
+                <h2 className="text-xl leading-none hover:text-secondaryGold text-center mx-auto">
+                  X (Twitter)
+                </h2>
+              </Link>
+              <Link
+                href={`https://t.me/bitvaultfinance`}
+                target="_blank"
+                passHref
+              >
+                <h2 className="text-xl leading-none hover:text-secondaryGold mx-auto">
+                  Telegram
+                </h2>
+              </Link>
+            </PopoverPanel>
+          </Popover>
+        </div>
+
+        <div className="w-40"></div>
+
       </div>
     </div>
   );
