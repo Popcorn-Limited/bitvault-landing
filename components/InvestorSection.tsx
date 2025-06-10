1b9f183ce163b00dca3d864260854765a2819532
RedVeil <leon.niesler@gmail.com>
2025-06-10 13:09:43 +0200

    added gemini

--- components/InvestorSection.tsx
+++ components/InvestorSection.tsx
@@ -3,7 +3,8 @@ export default function InvestorSection() {
     <div className="flex flex-col items-center justify-end">
       <h1 className="text-xl font-bold uppercase text-primaryGold">Backed by</h1>
       <div className="mt-8 mb-8 flex flex-col md:flex-row gap-8">
-        <img src="/images/investors/gsr.png" alt="gsr" className="h-8 w-full object-contain" /> 
+        <img src="/images/investors/gsr.png" alt="gsr" className="h-8 w-full object-contain" />
+        <img src="/images/investors/gemini.png" alt="gemini" className="h-8 w-full object-contain" />
         <img src="/images/investors/auros.png" alt="auros" className="h-6 w-full object-contain mt-1" /> 
         <img src="/images/investors/keyrock.png" alt="keyrock" className="h-8 w-full object-contain" /> 
         <img src="/images/investors/matrixport.png" alt="matrixport" className="h-8 w-full object-contain" /> 
