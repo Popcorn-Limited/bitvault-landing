f125fdbef69c211e660b74ab5fed9b1cb922008d
RedVeil <leon.niesler@gmail.com>
2025-06-19 10:22:43 +0200

    removed main button

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -18,9 +18,9 @@ export default function HeroSection(): JSX.Element {
         <div className="">
           <h1 className="text-white text-7xl md:text-8xl font-medium leading-none text-center">Institutional, <br /> Bitcoin-Backed USD</h1>
           <h2 className="text-gray-400 text-3xl leading-none text-center mt-4">A stablecoin earning secure, risk adjusted yield</h2>
-          <div className="mx-auto w-60 mt-8">
+          {/* <div className="mx-auto w-60 mt-8">
             <MainActionButton label="Launch App" handleClick={() => router.push("https://app.bitvault.finance/")} />
-          </div>
+          </div> */}
         </div>
       </div>
 
