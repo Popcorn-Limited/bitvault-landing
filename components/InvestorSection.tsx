e18cca1cb685deb62478fc40d13a7130969467c8
Andrea <dinennoandrea@gmail.com>
2025-09-03 12:39:57 +0200

    Fix margin

--- components/InvestorSection.tsx
+++ components/InvestorSection.tsx
@@ -1,6 +1,6 @@
 export default function InvestorSection() {
   return (
-    <div className="flex flex-col items-center justify-end mt-60">
+    <div className="flex flex-col items-center justify-end mt-30">
       <h1 className="text-xl font-bold uppercase text-primaryGold">Backed by</h1>
       <div className="mt-8 mb-8 flex flex-col md:flex-row gap-8">
         <img src="/images/investors/gsr.png" alt="gsr" className="h-8 w-full object-contain" />
